7a0706d7d11c0f3668f3651cdd46170c4024e716
allenr17 <60397204+allenr17@users.noreply.github.com>
2022-04-18 19:37:18 -0400

    Add files via upload

--- arduino.py
+++ arduino.py
@@ -18,14 +18,14 @@ arduino.open()
 
 
 def gpSend(latitude, longitude):
-    url = "http://3301-152-17-173-84.ngrok.io/markers"
+    url = "https://trackberry-server.herokuapp.com/markers"
     data = {"type": "Feature",
             "geometry": {"type": "Point", "coordinates": [latitude, longitude]},
             "properties": {}
             }
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     res = requests.post(url, json=data)
-    #print (res)
+    print (res)
 
 
 def lat(deg, min):
@@ -36,45 +36,53 @@ def long(deg, min):
 
 
 while 1:
-    data = arduino.readline()
-    if data:
-        print(data)
-        tokens = data.decode('ISO-8859-1').split(',')
+    try:
+        data = arduino.readline()
+        if data:
+            print(data)
+            tokens = data.decode('ISO-8859-1').split(',')
 
-        if (tokens[0][0] == "L"):
-            tokens[1] = tokens[1].strip()
-            tokens[3] = tokens[3].strip()
-            latdeg = tokens[1][:2]
-            latmin = tokens[1][2:]
-            NS = tokens[2]
-            EW = tokens[4]
-            print(tokens)
-            print(latdeg)
-            print(latmin)
-            if(len(tokens[3]) == 10):
-                longdeg = tokens[3][:3]
-                longmin = tokens[3][3:]
-                print(longdeg)
-                print(longmin)
-            elif(len(tokens[3])==9):
-                longdeg = tokens[3][:2]
-                longmin = tokens[3][2:]
+            if (tokens[0][0] == "L"):
+                tokens[1] = tokens[1].strip()
+                tokens[3] = tokens[3].strip()
+                latdeg = tokens[1][:2]
+                latmin = tokens[1][2:]
+                NS = tokens[2]
+                EW = tokens[4]
+                #print(tokens)
+                #print(latdeg)
+                #print(latmin)
+                if(len(tokens[3]) == 10):
+                    longdeg = tokens[3][:3]
+                    longmin = tokens[3][3:]
+                    #print(longdeg)
+                    #print(longmin)
+                elif(len(tokens[3])==9):
+                    longdeg = tokens[3][:2]
+                    longmin = tokens[3][2:]
 
-                print(longdeg)
-                print(longmin)
+                    #print(longdeg)
+                    #print(longmin)
+                else:
+                    longdeg = tokens[3][:2]
+                    longmin = tokens[3][2:]
+                #print(tokens[4])
+                sendLat = (lat(float(latdeg), float(latmin)))
+                sendLong = (long(float(longdeg), float(longmin)))
+                #print(type(NS))
+                if ("S" in NS):
+                    sendLat = -1*sendLat
+                elif("W" in EW):
+                    sendLong = -1*sendLong
+                print(sendLat)
+                print(sendLong)
+                print(gpSend(sendLong, sendLat))
+                arduino.close()
+                time.sleep(60)
+                arduino.open()
+    except:
+        time.sleep(15)
 
-            #print(tokens[4])
-            sendLat = (lat(float(latdeg), float(latmin)))
-            sendLong = (long(float(longdeg), float(longmin)))
-            print(type(NS))
-            if ("S" in NS):
-                sendLat = -1*sendLat
-            elif("W" in EW):
-                sendLong = -1*sendLong
-            print(sendLat)
-            print(sendLong)
-            print(gpSend(sendLong, sendLat))
-            time.sleep(15)
 
 
 
