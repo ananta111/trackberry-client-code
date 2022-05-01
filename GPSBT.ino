79250490205067ab69e9f4c4aa57a83eab548f0d
allenr17 <60397204+allenr17@users.noreply.github.com>
2022-04-30 22:57:40 -0400

    Add files via upload

--- GPSBT.ino
+++ GPSBT.ino
@@ -1,14 +1,5 @@
-// Test code for Adafruit Flora GPS modules
-//
-// This code shows how to listen to the GPS module in an interrupt
-// which allows the program to have more 'freedom' - just parse
-// when a new NMEA sentence is available! Then access data when
-// desired.
-//
-// Tested and works great with the Adafruit Flora GPS module
-// ------> http://adafruit.com/products/1059
-// Pick one up today at the Adafruit electronics shop
-// and help support open source hardware & software! -ada
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 #include <SoftwareSerial.h>
 
@@ -17,56 +8,77 @@ SoftwareSerial GPSSerial(9, 10); // RX, TX
   
 #include <Adafruit_GPS.h>
 #include <SoftwareSerial.h>
+
 Adafruit_GPS GPS(&GPSSerial);
+
 // Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
 // Set to 'true' if you want to debug and listen to the raw GPS sentences
 #define GPSECHO false
 // this keeps track of whether we're using the interrupt
 // off by default!
 boolean usingInterrupt = false;
+
+uint32_t timer = millis();
+float latitude;
+float longitude;
+char lat;
+char lon;
+
+float testlat[] = {
+  36.13403869374745, //0: reynolda hall -- side stairs
+  36.13407769957198, //1: reynolda hall -- top of stairs
+  36.13390005112619, //2: reynolda hall -- front sidewalk
+  36.133797722934105,//3: reynolda hall -- right out front
+  36.13369730391507, //4: reynolda hall -- on location
+  36.13581239439343, //5: wakerspace
+  36.135299190856244 //6: chapel
+  
+};
+
+float testlon[] = {
+  80.27719713212093,
+  80.27738731358706,
+  80.27745337662266,
+  80.27756664171382,
+  80.27747768049953,
+  80.28091585407873,
+  80.27906573915102
+};
+int testlocation = 5;
+
+float latitudes[] = {
+  36.13369730391507, //reynolda hall
+  36.137113596106744, //starbucks
+  36.13550713600733, //gravel lot
+  36.133478462875104 //manchester
+};
+float longitudes[] = {
+  -80.27747768049953,
+  -80.27952469795977,
+  -80.28104438125831,
+  -80.27649646356159
+};
+int numLocations = 4;
+float threshold = 0.03;                                   //settings
+float currDistance;
+
+const int buzzer = 6;
+
 void setup()
 {
-// connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-// also spit it out
-Serial.begin(115200);
-Serial.println("Adafruit GPS library basic test!");
-// 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-GPS.begin(9600);
-// uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-// uncomment this line to turn on only the "minimum recommended" data
-//GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-// For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-// the parser doesn't care about other sentences at this time
-// Set the update rate
-GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
-// For the parsing code to work nicely and have time to sort thru the data, and
-// print it out we don't suggest using anything higher than 1 Hz
-delay(1000);
-// Ask for firmware version
-Serial1.println(PMTK_Q_RELEASE);
+  pinMode(buzzer, OUTPUT); 
+  Serial.begin(115200);
+  GPS.begin(9600);
+  delay(1000);
 }
-uint32_t timer = millis();
+
+
 void loop() // run over and over again
 {
-  // read data from the GPS in the 'main loop'
-  char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
-  if (GPSECHO)
-  if (c) Serial.print(c);
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-  // a tricky thing here is if we print the NMEA sentence, or data
-  // we end up not listening and catching other sentences!
-  // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
-  Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-  if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-    return; // we can fail to parse a sentence in which case we should just wait for another
-  }
-  // if millis() or timer wraps around, we'll just reset it
+
   if (timer > millis()) timer = millis();
   // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) {
+  if (millis() - timer > 5000) {
     timer = millis(); // reset the timer
     Serial.print("\nTime: ");
     Serial.print(GPS.hour, DEC); Serial.print(':');
@@ -79,200 +91,101 @@ void loop() // run over and over again
     Serial.println(GPS.year, DEC);
     Serial.print("Fix: "); Serial.print((int)GPS.fix);
     Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-    //if (GPS.fix) {
-      Serial.print("Location: ,");
-      Serial.print(GPS.latitude, 4); 
-      Serial.print(", ");
-      Serial.print(GPS.lat);
-      Serial.print(", ");
-      Serial.print(GPS.longitude, 4); 
-      Serial.print(", ");
-      Serial.println(GPS.lon);
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-  //}
-}
-}
-
-
-
-/*// Test code for Ultimate GPS Using Hardware Serial (e.g. GPS Flora or FeatherWing)
-//
-// This code shows how to listen to the GPS module via polling. Best used with
-// Feathers or Flora where you have hardware Serial and no interrupt
-//
-// Tested and works great with the Adafruit GPS FeatherWing
-// ------> https://www.adafruit.com/products/3133
-// or Flora GPS
-// ------> https://www.adafruit.com/products/1059
-// but also works with the shield, breakout
-// ------> https://www.adafruit.com/products/1272
-// ------> https://www.adafruit.com/products/746
-//
-// Pick one up today at the Adafruit electronics shop
-// and help support open source hardware & software! -ada
-#include <SoftwareSerial.h>
-
-SoftwareSerial GPSSerial(9, 10); // RX, TX
-#include <Adafruit_GPS.h>
-
-// what's the name of the hardware serial port?
-//#define GPSSerial Serial1
-
-// Connect to the GPS on the hardware port
-Adafruit_GPS GPS(&GPSSerial);
-
-// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
-// Set to 'true' if you want to debug and listen to the raw GPS sentences
-#define GPSECHO false
-
-uint32_t timer = millis();
-
-
-void setup()
-{
-  //while (!Serial);  // uncomment to have the sketch wait until Serial is ready
-
-  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
-  // also spit it out
-  Serial.begin(115200);
-  Serial.println("Adafruit GPS library basic parsing test!");
-
-  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
-  GPS.begin(9600);
-  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
-  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
-  // uncomment this line to turn on only the "minimum recommended" data
-  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
-  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
-  // the parser doesn't care about other sentences at this time
-  // Set the update rate
-  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ); // 1 Hz update rate
-  // For the parsing code to work nicely and have time to sort thru the data, and
-  // print it out we don't suggest using anything higher than 1 Hz
-
-  // Request updates on antenna status, comment out to keep quiet
-  GPS.sendCommand(PGCMD_ANTENNA);
-
-  delay(1000);
-
-  // Ask for firmware version
-  GPSSerial.println(PMTK_Q_RELEASE);
-}
-
-void loop() // run over and over again
-{
-  // read data from the GPS in the 'main loop'
-  char c = GPS.read();
-  // if you want to debug, this is a good time to do it!
-  if (GPSECHO)
-    if (c) Serial.print(c);
-  // if a sentence is received, we can check the checksum, parse it...
-  if (GPS.newNMEAreceived()) {
-    // a tricky thing here is if we print the NMEA sentence, or data
-    // we end up not listening and catching other sentences!
-    // so be very wary if using OUTPUT_ALLDATA and trying to print out data
-    Serial.print(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
-    if (!GPS.parse(GPS.lastNMEA())) // this also sets the newNMEAreceived() flag to false
-      return; // we can fail to parse a sentence in which case we should just wait for another
-  }
-
-  // approximately every 2 seconds or so, print out the current stats
-  if (millis() - timer > 2000) {
-    timer = millis(); // reset the timer
-    Serial.print("\nTime: ");
-    if (GPS.hour < 10) { Serial.print('0'); }
-    Serial.print(GPS.hour, DEC); Serial.print(':');
-    if (GPS.minute < 10) { Serial.print('0'); }
-    Serial.print(GPS.minute, DEC); Serial.print(':');
-    if (GPS.seconds < 10) { Serial.print('0'); }
-    Serial.print(GPS.seconds, DEC); Serial.print('.');
-    if (GPS.milliseconds < 10) {
-      Serial.print("00");
-    } else if (GPS.milliseconds > 9 && GPS.milliseconds < 100) {
-      Serial.print("0");
-    }
-    Serial.println(GPS.milliseconds);
-    Serial.print("Date: ");
-    Serial.print(GPS.day, DEC); Serial.print('/');
-    Serial.print(GPS.month, DEC); Serial.print("/20");
-    Serial.println(GPS.year, DEC);
-    Serial.print("Fix: "); Serial.print((int)GPS.fix);
-    Serial.print(" quality: "); Serial.println((int)GPS.fixquality);
-    if (GPS.fix) {
-      Serial.print("Location: ");
-      Serial.print(GPS.latitude, 4); Serial.print(GPS.lat);
-      Serial.print(", ");
-      Serial.print(GPS.longitude, 4); Serial.println(GPS.lon);
-      Serial.print("Speed (knots): "); Serial.println(GPS.speed);
-      Serial.print("Angle: "); Serial.println(GPS.angle);
-      Serial.print("Altitude: "); Serial.println(GPS.altitude);
-      Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
-    }
-  }
-}*/
-/*#include <Adafruit_NeoPixel.h>
-
-#define PIN 8
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(1, PIN, NEO_GRB + NEO_KHZ800);
-
-void setup() {
-  strip.begin();
-  strip.setBrightness(50);
-  strip.show(); // Initialize all pixels to 'off'
-  //pinMode(led, OUTPUT);  
-}
-
-void loop() {
-  // Some example procedures showing how to display to the pixels:
-
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);               // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);               // wait for a second
   
-  colorWipe(strip.Color(255, 0, 0), 500); // Red
-  colorWipe(strip.Color(0, 255, 0), 500); // Green
-  colorWipe(strip.Color(0, 0, 255), 500); // Blue
-  rainbowCycle(20);
-}
+    Serial.print("Location: ,");
+    latitude = GPS.latitude;
+    longitude = GPS.longitude;
+    lat = GPS.lat;
+    lon = GPS.lon;
+    Serial.print(GPS.latitude, 4); 
+    Serial.print(", ");
+    Serial.print(GPS.lat);
+    Serial.print(", ");
+    Serial.print(GPS.longitude, 4); 
+    Serial.print(", ");
+    Serial.println(GPS.lon);
+   }  
+   
+   //start testing code for no GPS connection
+   latitude = testlat[testlocation];
+   longitude = testlon[testlocation];
+   lat = 'N';
+   lon = 'W';
+   //end testing code
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
-  }
-}
+   Serial.println(latitude);
+   Serial.println(lat);
+   Serial.println(longitude);
+   Serial.println(lon);
+   if (lat == 'S'){
+    latitude = -1*latitude;
+   }
+   if(lon == 'W'){
+    longitude = -1*longitude;
+   }
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
+  Serial.println(latitude, 10);
 
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+   bool perimiter = checkDist(latitude, longitude);
+
+   Serial.println(perimiter);
+   if (perimiter){
+    tone(buzzer, 1000); // Send 1KHz sound signal...
+    delay(100);        // ...for 1 sec
+    if (currDistance > 0){
+      noTone(buzzer);
     }
-    strip.show();
-    delay(wait);
-  }
+    delay(currDistance*100000);
+    Serial.println(currDistance, 4);
+   }
+  
+  delay(100); 
 }
 
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else if(WheelPos < 170) {
-    WheelPos -= 85;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+bool checkDist(float currLat, float currLon){
+  for (int i = 0; i<numLocations; i++){
+    float dist = calcDist(currLat, currLon, i);
+    if (dist <= threshold){
+      currDistance = dist;
+      return true;
+    }
   }
-}*/
+  return false;
+};
+
+float calcDist(float currLat, float currLon, int loc){
+  float lat1 = toRadians(latitudes[loc]);
+  float long1 = toRadians(longitudes[loc]);
+  float lat2 = toRadians(currLat);
+  float long2 = toRadians(currLon);
+
+  // Haversine Formula
+  float dlong = long2 - long1;
+  float dlat = lat2 - lat1;
+
+  float ans = pow(sin(dlat / 2), 2) +
+      cos(lat1) * cos(lat2) *
+      pow(sin(dlong / 2), 2);
+
+  ans = 2 * asin(sqrt(ans));
+
+  // Radius of Earth in
+  // Kilometers, R = 6371
+  // Use R = 3956 for miles
+  float R = 3956;
+
+  // Calculate the result
+  ans = ans * R;
+
+  return ans;
+};
+
+float toRadians(const float degree)
+{
+    // cmath library in C++
+    // defines the constant
+    // M_PI as the value of
+    // pi accurate to 1e-30
+    float one_deg = (M_PI) / 180;
+    return (one_deg * degree);
+}
